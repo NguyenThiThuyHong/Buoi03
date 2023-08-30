05aff54edc4cb89d39af9e6e60bdedb9d7770cfa
Hong <thuyhong111202@gmail.com>
2023-08-30 14:23:10 +0700

    BTBuoi3

--- BTBuoi3/BTBuoi3/HangThucPham.cs
+++ BTBuoi3/BTBuoi3/HangThucPham.cs
@@ -9,10 +9,45 @@ namespace BTBuoi3
     class HangThucPham
     {
         private string MaHang;
+        public string MaHang1
+        {
+            get
+            {
+                return MaHang;
+            }
+            set
+            {
+                MaHang = value;
+            }
+        }
+
         private string TenHang;
+        public string TenHang1
+        {
+            get { return TenHang; }
+            set { TenHang = value; }
+        }
+
         private int DonGia;
+        public int DonGia1
+        {
+            get { return DonGia; }
+            set { DonGia = value; }
+        }
+
         private bool NgaySX;
+        public bool NgaySX1
+        {
+            get { return NgaySX; }
+            set { NgaySX = value; }
+        }
+
         private bool NgayHH;
+        public bool NgayHH1
+        {
+            get { return NgayHH; }
+            set { NgayHH = value; }
+        }
 
 
     }
